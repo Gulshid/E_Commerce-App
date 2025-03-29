6f1f7a8c40b45dcfab89bf2018a1b535efada902
Gulshid Zada <gulshidzada4@gmail.com>
2025-03-29 16:57:16 +0500

    Completed  Successfully

--- flutter_structure_design/lib/view_/home_view.dart
+++ flutter_structure_design/lib/view_/home_view.dart
@@ -1,14 +1,11 @@
-// import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:e_commerce_app/view_/Drawer_menu.dart';
 import 'package:e_commerce_app/view_/bottom_bar.dart';
-// import 'package:e_commerce_app/view_/bottom_bar.dart';
 import 'package:e_commerce_app/view_/home.dart';
 import 'package:e_commerce_app/view_/whishlists.dart';
 import 'package:e_commerce_app/view_/Cart.dart';
 import 'package:e_commerce_app/view_/setting.dart';
 import 'package:e_commerce_app/view_Model/E_Provider.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 class HomeView extends StatefulWidget {
